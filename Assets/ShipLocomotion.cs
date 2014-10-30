cb5a1a237cb3d1f292420e5d871899526f220762
Luca <lucaleone@outlook.com>
2014-10-30 16:50:19 +0100

    upload project

---
+++ Assets/ShipLocomotion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShipLocomotion : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public float rotationSpeed = 3.0f;
+    public Transform explosion;
+    bool alive = true;
+    void FixedUpdate()
+    {
+        if (alive)
+        {
+            Vector3 tergetvelocity = (transform.forward * speed * Input.GetAxis("Vertical")) - rigidbody.velocity;
+
+            tergetvelocity.x = Mathf.Clamp(tergetvelocity.x, -maxVelocityChange, maxVelocityChange);
+            tergetvelocity.z = Mathf.Clamp(tergetvelocity.z, -maxVelocityChange, maxVelocityChange);
+            rigidbody.AddForce(tergetvelocity, ForceMode.VelocityChange);
+            transform.Rotate(0, rotationSpeed * Input.GetAxis("Horizontal"), 0);
+            rigidbody.MoveRotation(Quaternion.Euler(0, transform.rotation.eulerAngles.y, -25 * Input.GetAxis("Horizontal")));
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        rigidbody.useGravity = true;
+        rigidbody.constraints = RigidbodyConstraints.None;
+        explosion.gameObject.SetActive(true);
+        alive = false;
+    }
+}
